7ed8e6fe537b3ee54635492e26d2053d692d7de7
Izabela <bellg477@gmail.com>
2023-09-17 03:02:51 +0300

    Added a new class and a loop

---
+++ src/main/java/com/supermarketapi/common/PurchaseAttributesMessages.java
@@ -0,0 +1,10 @@
+package com.supermarketapi.common;
+
+public class PurchaseAttributesMessages {
+    public static final String SUPERMARKET = "supermarket";
+    public static final String ITEMS = "items";
+    public static final String PAYMENT_TYPE = "paymentType";
+    public static final String TOTAL_CASH_AMOUNT = "totalCashAmount";
+    public static final String MONEY_GIVEN = "moneyGiven";
+    public static final String TIME_OF_PAYMENT = "timeOfPayment";
+}
